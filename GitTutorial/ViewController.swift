348a97596a814df7e95e95d308f571b1f3060a8a
Abdullah <abdullah.techcampus@mail.com>
2019-06-16 14:02:16 +0300

    viewController

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Printing
         print("Hello GitHub")
+        print("Trying the source contrl")
     }
 
 
